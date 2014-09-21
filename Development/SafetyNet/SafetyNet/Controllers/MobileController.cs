ee8293b10db9be8a785f6d9d5e69faf7405219e5
Michael Richardson <DragonRider0o0@gmail.com>
2014-09-21 01:33:24 -0400

    Added Mobile controller and views.

---
+++ Development/SafetyNet/SafetyNet/Controllers/MobileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SafetyNet.Controllers
+{
+    public class MobileController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("Alert");
+        }
+        // GET: Mobile
+        public ActionResult Alert()
+        {
+            return View("AlertView");
+        }
+        public ActionResult Calm()
+        {
+            return View();
+        }
+        public ActionResult Map()
+        {
+            return View();
+        }
+        public ActionResult Update()
+        {
+            return View();
+        }
+        public ActionResult Confirm()
+        {
+            return View();
+        }
+    }
+}
